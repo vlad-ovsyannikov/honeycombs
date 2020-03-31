88e3bf1246cf59c6e2c2ca9a5d47062c045e5a46
vladimir.ovsyanikov <vladimir.ovsyanikov@cloudcall.com>
2020-04-01 00:15:48 +0300

    Add prettier to webapp

--- services/hc-web/src/App.tsx
+++ services/hc-web/src/App.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
 
-import { Button } from 'semantic-ui-react'
-
-import 'semantic-ui-css/semantic.min.css'
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <Button>Hello Semantic UI</Button>
-      </header>
+    <div className="wrapper">
+      <div>learn react</div>
     </div>
   );
-}
+};
 
 export default App;
